f62559f01969543a6d527f4e3b784b2874073d4c
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-01-30 00:02:16 -0300

    commit inicial - estruturaçao de projeto

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header />
   <router-view/>
 </template>
 
+<script>
+import Header from './components/Header.vue'
+
+export default{
+  components:{
+    Header
+  }
+}
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  *{
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box
+  }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
