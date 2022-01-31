b4d1485702d313c4b1fd9e1d400ef7a150350578
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-01-31 17:51:07 -0300

    iniciando componente de filtro de pesquisa

---
+++ src/components/Search.vue
@@ -0,0 +1,52 @@
+<template>
+<div class = "container-filter">
+
+    <div class="input-search" id = "search-imovel">
+                <input id = "input-search" type="text" placeholder="Pesquise aqui" v-model="search">
+    </div>
+
+    <div id = "filter-price">
+                <select id = "select-price" v-model="selected">
+                    <option selected>Filtrar por preço</option>
+                    <option value = 1>Mais caros</option>
+                    <option value = 0>Mais baratos</option>
+                </select>
+    </div>
+
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'Search'
+}
+</script>
+
+<style scoped>
+
+    .container-filter{
+        width: 50%;
+        margin-bottom: 10px;
+    }
+
+    #search-imovel{ 
+        float: left;
+        width: 50.75%;
+    }
+
+    #input-search{
+        width: 95%
+    }
+
+    #filter-price{ 
+        display: inline-block;
+        margin: 0 auto;
+    }
+
+    #select-price, #input-search{
+        height: 30px;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="main-container">
+    <Search/>
     <Card/> 
   </div>
 </template>
 
 <script>
+import Search from '../components/Search.vue'
 import Card from '../components/Card.vue'
 export default {
   name: 'Home',
   components:{
+    Search,
     Card
   }
 }
