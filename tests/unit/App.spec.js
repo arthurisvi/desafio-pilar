4ed7f00bc0f81339ec6e0f911414175afb884a96
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-02-04 14:08:42 -0300

    configuraçoes de teste ok

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -2,8 +2,8 @@ import { shallowMount } from "@vue/test-utils";
 import App from "../../src/App";
 
 describe("App", () => {
-    test("é uma instância Vue", () => {
+    it("é uma instância Vue", () => {
         const wrapper = shallowMount(App);
-        expect(wrapper.isVueInstance()).toBeTruthy();
+        expect(wrapper.exists()).toBeTruthy();
     });
 });
